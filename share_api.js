2c2e5e9526538cbb01854eac3012d0072f7c0a42
Youssef Bouali <53417796+youssefbouali@users.noreply.github.com>
2021-05-21 22:35:26 +0100

    Create share_api.js

---
+++ share_api.js
@@ -0,0 +1,3 @@
+function share_post(text) {
+	window.open("https://www.th3arabe.ml/share.php?text="+text, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=150,left=250,width=700,height=500");
+}
