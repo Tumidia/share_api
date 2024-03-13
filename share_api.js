d8322760d019d11eaa4c53031487301b8d304116
Youssef Bouali <53417796+youssefbouali@users.noreply.github.com>
2024-03-13 16:46:14 +0000

    Update share_api.js

--- share_api.js
+++ share_api.js
@@ -1,3 +1,3 @@
 function share_post(text) {
-	window.open("https://www.th3arabe.ml/share.php?text="+text, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=150,left=250,width=700,height=500");
+	window.open("https://tumidia.wsoum.eu.org/share.php?text="+text, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=150,left=250,width=700,height=500");
 }
